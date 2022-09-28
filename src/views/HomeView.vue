a9ba5a5222ed009b118b193548d97e734a83c7e6
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-28 14:57:28 +0700

    [Update] Watch and WatchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,13 @@
     <p>Search Term - {{ search }}</p>
 
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+
+    <button @click="handleClick">Stop</button>
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -18,11 +20,24 @@ export default {
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi'])
 
+    const stopWatch = watch(search, () => {
+      console.log('watch Func')
+    }) 
+
+    const stopEffect = watchEffect(() => {
+      console.log('WatchEffect', search.value)
+    })
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value))
     })
 
-    return { names, search, matchingNames }
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    return { names, search, matchingNames, handleClick }
   }
 }
 </script>
