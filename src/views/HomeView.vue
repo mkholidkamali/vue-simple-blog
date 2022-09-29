0ba2c962efdd7665d0a3ff11394027e433f92cfe
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-29 11:08:07 +0700

    [Update] Composable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,33 +10,18 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts"
 
 export default {
   name: "HomeView",
   components: { PostList },
   setup() {
-      const posts = ref([])
-      const error = ref(null)
+    const { posts, error, load } = getPosts()
 
-      const load = async () => {
-        try {
-          let data = await fetch('http://localhost:3000/posts')
-          if(!data.ok) {
-            throw Error('no data available')
-          }
-          posts.value = await data.json()
-        }
-        catch(err) {
-          error.value = err.message
-          console.log(error.value)
-        }
-      }
+    load()
 
-      load()
-
-      return { posts, error };
+    return { posts, error };
   },
 }
 </script>
