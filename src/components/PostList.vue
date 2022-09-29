eca39820960118a9f3f9c121da9282914c07e338
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-29 09:12:36 +0700

    [Update] Props Setup && Lifecycle

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core';
+import SinglePost from "./SinglePost.vue";
+export default {
+    props: ["posts"],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() => {console.log('mounted')})
+        onUnmounted(() => {console.log('Unmounted')})
+        onUpdated(() => {console.log('Updated')})
+    }
+}
+</script>
+
+<style>
+
+</style>
