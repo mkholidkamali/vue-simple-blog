ecb5efd7a34b7c5c173122018e3daa10e327e646
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-29 09:55:25 +0700

    [Update] Fetching Data in Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts"></PostList>
-    <button @click="showPosts = !showPosts">Toggle</button>
-    <button @click="posts.pop()">Delete</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"></PostList>
+    </div>
+    <div v-else>Loading ...</div>
   </div>
 </template>
 
@@ -15,14 +17,26 @@ export default {
   name: "HomeView",
   components: { PostList },
   setup() {
-      const posts = ref([
-          { title: "Welcome to the blog", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Platea dictumst vestibulum rhoncus est pellentesque. Velit ut tortor pretium viverra suspendisse potenti nullam ac. Pharetra convallis posuere morbi leo urna. Aliquam ut porttitor leo a diam sollicitudin tempor id. Quam quisque id diam vel quam elementum pulvinar. Elit duis tristique sollicitudin nibh sit amet. Quis risus sed vulputate odio ut enim blandit volutpat. Suspendisse in est ante in. In vitae turpis massa sed elementum tempus egestas sed. Facilisis mauris sit amet massa vitae. Est placerat in egestas erat. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris.", id: 1 },
-          { title: "Top 5 css tips", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Platea dictumst vestibulum rhoncus est pellentesque. Velit ut tortor pretium viverra suspendisse potenti nullam ac. Pharetra convallis posuere morbi leo urna. Aliquam ut porttitor leo a diam sollicitudin tempor id. Quam quisque id diam vel quam elementum pulvinar. Elit duis tristique sollicitudin nibh sit amet. Quis risus sed vulputate odio ut enim blandit volutpat. Suspendisse in est ante in. In vitae turpis massa sed elementum tempus egestas sed. Facilisis mauris sit amet massa vitae. Est placerat in egestas erat. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris.", id: 2 }
-      ]);
+      const posts = ref([])
+      const error = ref(null)
 
-      const showPosts = ref(true)
+      const load = async () => {
+        try {
+          let data = await fetch('http://localhost:3000/posts')
+          if(!data.ok) {
+            throw Error('no data available')
+          }
+          posts.value = await data.json()
+        }
+        catch(err) {
+          error.value = err.message
+          console.log(error.value)
+        }
+      }
 
-      return { posts, showPosts };
+      load()
+
+      return { posts, error };
   },
 }
 </script>
